d86e8ea50581fd69ff3174a264fe576bf4e9a810
BirdyBird <birdyDixon@hotmail.com>
2026-01-22 16:54:07 -0500

    Added support for .mid file extension.

--- src/main/java/net/conczin/data/MelodyAsset.java
+++ src/main/java/net/conczin/data/MelodyAsset.java
@@ -13,6 +13,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.List;
 
 public class MelodyAsset implements JsonAssetWithMap<String, DefaultAssetMap<String, MelodyAsset>> {
     public static final AssetBuilderCodec<String, MelodyAsset> CODEC = AssetBuilderCodec.builder(
@@ -29,18 +30,39 @@ public class MelodyAsset implements JsonAssetWithMap<String, DefaultAssetMap<Str
                     (item, v) -> item.name = v,
                     item -> item.name,
                     (item, parent) -> item.name = parent.name
-            )
+            )            
             .add()
+            // New JSON value which tells the parser to trim whitespace at the front of the MIDI.
+            //.appendInherited(
+        		//	new KeyedCodec<>("TrimMIDI", Codec.BOOLEAN),
+            //   (item, v) -> item.offsetNotes = v,
+            //   item -> item.offsetNotes,
+            //    (item, parent) -> item.offsetNotes = parent.offsetNotes
+        		//)
+            //.add()
             .afterDecode((o, extrainfo) -> {
                 if (o.id != null && extrainfo instanceof AssetExtraInfo<?> assetExtraInfo) {
                     Path path = assetExtraInfo.getAssetPath();
                     if (path == null) return;
-                    Path midiPath = path.getParent().resolve(path.getFileName().toString().replaceFirst("\\.json$", ".midi"));
-                    try (InputStream midiStream = Files.newInputStream(midiPath)) {
-                        o.melody = new Melody(o.name, MidiParser.parseMidi(midiStream));
-                    } catch (IOException e) {
-                        throw new RuntimeException(e);
-                        // Nop
+                    Path basePath = path.getParent().resolve(path.getFileName().toString().replaceFirst("\\.json$", ""));
+                    List<Path> midiPaths = List.of(
+                    			basePath.resolveSibling(basePath.getFileName().toString() + ".midi"),
+                    			basePath.resolveSibling(basePath.getFileName().toString() + ".mid")
+                    		); // Support for both MIDI file extensions.
+                    //Path midiPath = path.getParent().resolve(path.getFileName().toString().replaceFirst("\\.json$", ".midi"));
+                    IOException prevException = null;
+                    for (Path midiPath : midiPaths) {
+                    	try (InputStream midiStream = Files.newInputStream(midiPath)) {
+                            o.melody = new Melody(o.name, MidiParser.parseMidi(midiStream));
+                            return;
+                        } catch (IOException e) {
+                        		if (prevException == null) {
+                        			prevException = e;
+                        		}
+                        		else {
+                        			throw new RuntimeException(e);
+                        		}
+                        }
                     }
                 }
             })
@@ -60,7 +82,7 @@ public class MelodyAsset implements JsonAssetWithMap<String, DefaultAssetMap<Str
     private AssetExtraInfo.Data data;
 
     private String name;
-
+    //private boolean offsetNotes; // New JSON value which tells the parser to trim whitespace at the front of the MIDI.
     private Melody melody;
 
     @Override
